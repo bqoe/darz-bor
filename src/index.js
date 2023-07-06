c20e02fb1295e16d850c48a146a1e6fb7037940d
berry <kathborowy@gmail.com>
2023-07-06 17:06:09 +0200

    js

---
+++ src/index.js
@@ -0,0 +1,16 @@
+let headerElement = document.querySelector("header")
+
+document.addEventListener("scroll", () => {
+    let scroll = document.documentElement.scrollTop;
+    if (scroll > 1) {
+        headerElement.classList.add('background-header');
+    } else {
+        headerElement.classList.remove('background-header');
+    }
+    
+})
+
+if (screen.width < 770) {
+    let dropdownElement = document.querySelector('a.menu-trigger');
+    
+}
