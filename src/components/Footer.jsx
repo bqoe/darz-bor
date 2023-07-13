73a2deef4f30a1a7e95919d7d6af30edb4ef96be
berry <kathborowy@gmail.com>
2023-07-13 23:45:28 +0200

    header as a component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,38 +1,50 @@
-import React from 'react';
-import '../styles/footer.scss';
+import React from "react";
+import "../styles/footer.scss";
 
 const Footer = () => {
-    return (
-        <div className="footers">
-            <div className="mainFooter py-5 container">
-                <div className="row">
-                    <div className="col-4">
-                        <h1 className='h1DarzBor'>Darz Bór</h1> 
-                    </div>
-                    <div className="col-4">
-                        <h1>Nawigacja</h1>
-                        <ul>
-                            <li>Strona Główna</li>
-                            <li>Historia</li>
-                            <li>Prawo</li>
-                            <li>Kontakt</li>
-                        </ul>
-                    </div>
-                    <div className="col-4">
-                        <h1>Kontakt</h1>
-                        <p>Koło Łowieckie "Darz Bór"</p>
-                        <p>ul. Jakaś</p>
-                        <p>38-500 Sanok</p>
-                        <p>email: </p>
-                    </div>
-                </div>
-            
-            </div>
-            <div className='postFooter'>
-                <p>© 2023 Koło Łowieckie Darz Bór. Realizacja - <a href="https://github.com/bqoe">bqoe</a></p>
-            </div>
+  return (
+    <div className="footers">
+      <div className="mainFooter py-5 container">
+        <div className="row">
+          <div className="col-4">
+            <h1 className="h1DarzBor">Darz Bór</h1>
+          </div>
+          <div className="col-4">
+            <h1>Nawigacja</h1>
+            <ul class="nav d-flex flex-column">
+              <li>
+                <a href="index.html" class="active">
+                  Strona Główna
+                </a>
+              </li>
+              <li>
+                <a href="./historia.html">Historia</a>
+              </li>
+              <li>
+                <a href="./prawo.html">Prawo</a>
+              </li>
+              <li>
+                <a href="./kontakt.html">Kontakt</a>
+              </li>
+            </ul>
+          </div>
+          <div className="col-4">
+            <h1>Kontakt</h1>
+            <p>Koło Łowieckie "Darz Bór"</p>
+            <p>ul. Jakaś</p>
+            <p>38-500 Sanok</p>
+            <p>email: </p>
+          </div>
         </div>
-    )
-}
+      </div>
+      <div className="postFooter">
+        <p>
+          © 2023 Koło Łowieckie Darz Bór. Realizacja -{" "}
+          <a href="https://github.com/bqoe">bqoe</a>
+        </p>
+      </div>
+    </div>
+  );
+};
 
 export default Footer;
--- src/index.js
+++ src/index.js
@@ -1,19 +1,10 @@
 import {createRoot } from 'react-dom/client';
-import Footer from './components/Footer'
+import Header from './components/Header';
+import Footer from './components/Footer';
 import "./styles/index.scss"
-// import './pages/historia.html'
 
-let headerElement = document.querySelector("header")
-
-document.addEventListener("scroll", () => {
-    let scroll = document.documentElement.scrollTop;
-    if (scroll > 1) {
-        headerElement.classList.add('background-header');
-    } else {
-        headerElement.classList.remove('background-header');
-    }
-    
-})
+const headerRoot = createRoot(document.getElementById('header'));
+headerRoot.render(<Header />);
 
 const footerRoot = createRoot(document.getElementById('footer'));
-footerRoot.render(<Footer />)
+footerRoot.render(<Footer />);
