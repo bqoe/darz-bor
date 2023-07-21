3dada7495695cee76166c04d571fb5c3a723f600
berry <kathborowy@gmail.com>
2023-07-21 13:46:18 +0200

    ignore case

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import {createRoot } from 'react-dom/client';
-import Header from './components/Header';
-import Footer from './components/Footer';
+import Header from './components/header';
+import Footer from './components/footer';
 import "./styles/index.scss"
 
 const headerRoot = createRoot(document.getElementById('header'));
